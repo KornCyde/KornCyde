73a24ef5ac2f3fbacda5d458f691291ff67ffc8d
KornCyde <papalink@hotmail.com>
2022-08-16 11:36:03 -0500

    Archivo de prueba

---
+++ pasantia.php
@@ -0,0 +1,10 @@
+
+<?php  
+echo "Nombre: Jean Erreyes
+Telefono: 0994891161
+Correo: papalink@hotmail.com
+Lenguajes: PHP 
+BD : MYSQL, POSG
+
+Expectativas: -------
+Ganar un mayor conocimiento en base a lo aprendido";
