5ad49e2ca0d326dbd2779673a31115bc039dbd68
it@cateringclp.com <it@cateringclp.com>
2022-08-16 11:49:43 -0500

    style de info

--- pasantia.php
+++ pasantia.php
@@ -1,10 +1,93 @@
 
-<?php  
-echo "Nombre: Jean Erreyes
-Telefono: 0994891161
-Correo: papalink@hotmail.com
-Lenguajes: PHP 
-BD : MYSQL, POSG
+<html>
+  <head>
+   <link href="https://fonts.googleapis.com/css?family=Lobster|Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 
-Expectativas: -------
-Ganar un mayor conocimiento en base a lo aprendido";
+
+  </head>
+<body>
+<div class="float">
+  <h1>elShunior</h1>
+</div>
+<div>
+  <div class="card">
+    <img src="https://tuaptitud.com/wp-content/uploads/2017/02/avatar-default.png" alt="" />
+    <h1>Este es el titulo</h1>
+    <p>Lorem ipsum dolor sit ametdolor sit amet</p>
+    <a href="" class="btndetalles">Detalles</a>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img src="https://materiell.com/wp-content/uploads/2015/04/puma-full.png" alt="" />
+    <h1>Este es el titulo</h1>
+    <p>Lorem ipsum dolor sit ametdolor sit amet</p>
+    <a href="" class="btndetalles">Detalles</a>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img src="http://picot.shost.ca/img/logo.png" alt="" />
+    <h1>Este es el titulo</h1>
+    <p>Lorem ipsum dolor sit ametdolor sit amet</p>
+    <a href="" class="btndetalles">Detalles</a>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img src="https://blog.teachlr.com/wp-content/uploads/2016/06/hero-avatar.png" alt="" />
+    <h1>Este es el titulo</h1>
+    <p>Lorem ipsum dolor sit ametdolor sit amet</p>
+    <a href="" class="btndetalles">Detalles</a>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img src="https://materiell.com/wp-content/uploads/2015/03/doug_full1.png" alt="" />
+    <h1>Este es el titulo</h1>
+    <p>Lorem ipsum dolor sit ametdolor sit amet</p>
+    <a href="" class="btndetalles">Detalles</a>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img src="https://materiell.com/wp-content/uploads/2015/03/john-full.png" alt="" />
+    <h1>Este es el titulo</h1>
+    <p>Lorem ipsum dolor sit ametdolor sit amet</p>
+    <a href="" class="btndetalles">Detalles</a>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img src="http://images4.wikia.nocookie.net/inazuma/es/images/3/3b/Masato_4_2.png" alt="" />
+    <h1>Este es el titulo</h1>
+    <p>Lorem ipsum dolor sit ametdolor sit amet</p>
+    <a href="" class="btndetalles">Detalles</a>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img src="http://i0b.3djuegos.com/juegos/4690/inazuma_eleven_2/fotos/set/inazuma_eleven_2-1951345.jpg" alt="" />
+    <h1>Este es el titulo</h1>
+    <p>Lorem ipsum dolor sit ametdolor sit amet</p>
+    <a href="" class="btndetalles">Detalles</a>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img src="http://foro.recuperarelpelo.com/download/file.php?avatar=45156_1469718377.png" alt="" />
+    <h1>Este es el titulo</h1>
+    <p>Lorem ipsum dolor sit ametdolor sit amet</p>
+    <a href="" class="btndetalles">Detalles</a>
+  </div>
+</div>
+</body>
+  <br />
+  <div class="sharethis-inline-share-buttons"></div>
+  <script type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=58c3ba258338640012b7fdb5&product=inline-share-buttons"></script>
+
+
+</html>
